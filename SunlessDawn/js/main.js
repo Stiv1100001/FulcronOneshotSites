383b55007c4247ec98b90535ab38dc4d510c5d52
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-02-24 22:30:53 +0100

    update

--- SunlessDawn/js/main.js
+++ SunlessDawn/js/main.js
@@ -1,14 +1,28 @@
 const click = new Howl({
-	src: ['sound/lamp-click.mp3'],
+  src: ['sound/lamp-click.mp3'],
+  volume: 0.5,
 });
 
-document.querySelector('.hover .cord').addEventListener(
-	'click',
-	() => {
-		click.play();
-		click.on('end', () => {
-			document.getElementById('hover').classList.add('invisible');
-		});
-	},
-	{ once: true }
+document.querySelector('.hover #switch').addEventListener(
+  'click',
+  () => {
+    click.play();
+    click.on('end', () => {
+      document.getElementById('hover').classList.add('invisible');
+    });
+  },
+  { once: true }
 );
+
+/* Menu behavior */
+document.querySelectorAll('.nav-item').forEach((element) => {
+  element.addEventListener('click', (event) => {
+    let target = event.target;
+
+    document.querySelectorAll('.nav-item a').forEach((item) => {
+      item.classList.remove('active');
+    });
+
+    target.classList.add('active');
+  });
+});
